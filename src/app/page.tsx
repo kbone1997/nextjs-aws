0e7bb351b15580e39f1138fb528fed600766ab88
kbone1997 <doha123khan@gmail.com>
2024-12-03 01:43:36 +0600

    responsive check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -185,7 +185,7 @@ export default function Home() {
                 Continue with Google
               </div>
 
-              <div
+              <button
                 className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
                 onClick={handleGitHubSignIn}
               >
@@ -197,7 +197,7 @@ export default function Home() {
                   height={20}
                 />
                 Continue with GitHub
-              </div>
+              </button>
             </>
           ) : (
             <div
