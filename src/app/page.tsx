fc94c8d8a0500215a2b8eea221084ae63585733f
kbone1997 <doha123khan@gmail.com>
2024-12-03 01:58:19 +0600

    updated auth page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 items-center flex-col">
           {!isLoggedIn ? (
             <>
               <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-4">
@@ -186,7 +186,7 @@ export default function Home() {
               </div>
 
               <button
-                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] w-[200px] h-14 px-4 py-2"
                 onClick={handleGitHubSignIn}
               >
                 <Image
@@ -201,7 +201,7 @@ export default function Home() {
             </>
           ) : (
             <div
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-red-500 text-white gap-2 hover:bg-red-700 dark:hover:bg-red-400 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-red-500 text-white gap-2 hover:bg-red-700 dark:hover:bg-red-400 text-sm h-14"
               onClick={handleLogout}
             >
               Logout
